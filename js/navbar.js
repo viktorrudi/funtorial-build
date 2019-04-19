db99ab3558138335ef08b10181677947d948af86
viktorrudi <vrudi91@gmail.com>
2019-04-19 13:56:32 +0200

    worked on nav. only missing flags object loadin

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,6 @@
-const navbarFunc = function(){
-//Bootrap dropdown toggle
-document.querySelector('.dropdown-toggle').dropdown();
-
-const dropdownWrapper = document.querySelector('.dropdown');
-
-const currentLangSelected = document.createElement('a'),
-      dropdownMenu = document.createElement('div');
-
-      
-
+const navbarFunc = function () {
+  //Bootrap dropdown toggle
+  // document.querySelector('.dropdown-toggle').dropdown();
 }
 
 navbarFunc();
