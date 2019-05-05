7f9a4786b123c1157c2c2cd33c2cfe4e2bc1cf7d
Viktor Rudi <viktor@Rudey.local>
2019-05-05 19:26:58 +0200

    statistics dummy data

--- js/insert_data/statistics.js
+++ js/insert_data/statistics.js
@@ -7,7 +7,7 @@ export default [
     icon: 'img/flags/gb.svg',
     mainStats: {
       skillLevel: 'A1',
-      timeSpent: '22h 15m',
+      timeSpent: '23m',
       correctAnswers: 5,
       incorrectAnswers: 15,
       successRatio: 15,
@@ -30,13 +30,13 @@ export default [
   },
   {
     id: 2,
-    category: 'quiz',
-    title: 'English',
-    fullTitle: 'Example',
-    icon: 'img/flags/gb.svg',
+    category: 'language',
+    title: 'German',
+    fullTitle: 'German',
+    icon: 'img/flags/de.svg',
     mainStats: {
-      skillLevel: 'A1',
-      timeSpent: '22h 15m',
+      skillLevel: 'B2',
+      timeSpent: '2min',
       correctAnswers: 5,
       incorrectAnswers: 15,
       successRatio: 15,
@@ -59,15 +59,271 @@ export default [
   },
   {
     id: 3,
-    category: 'school-topic',
-    title: 'English',
-    fullTitle: 'Example',
-    icon: 'img/flags/gb.svg',
+    category: 'language',
+    title: 'French',
+    fullTitle: 'French',
+    icon: 'img/flags/fr.svg',
     mainStats: {
-      skillLevel: 'A1',
+      skillLevel: 'A2',
       timeSpent: '22h 15m',
       correctAnswers: 5,
       incorrectAnswers: 15,
+      successRatio: 15,
+      totalCredits: 523,
+    },
+    otherStats: [
+      {
+        stat: 52,
+        description: 'CUSTOM STAT',
+      },
+      {
+        stat: 22,
+        description: 'CUsdfsdfSTOM STAT',
+      },
+      {
+        stat: 5082,
+        description: 'CUSTOM STAasvT',
+      },
+    ],
+  },
+  {
+    id: 4,
+    category: 'language',
+    title: 'Japanese',
+    fullTitle: 'Japanese',
+    icon: 'img/flags/jp.svg',
+    mainStats: {
+      skillLevel: 'A1',
+      timeSpent: '3h 25m',
+      correctAnswers: 5,
+      incorrectAnswers: 15,
+      successRatio: 15,
+      totalCredits: 523,
+    },
+    otherStats: [
+      {
+        stat: 52,
+        description: 'CUSTOM STAT',
+      },
+      {
+        stat: 22,
+        description: 'CUsdfsdfSTOM STAT',
+      },
+      {
+        stat: 5082,
+        description: 'CUSTOM STAasvT',
+      },
+    ],
+  },
+  {
+    id: 5,
+    category: 'language',
+    title: 'Norwegian',
+    fullTitle: 'Norwegian',
+    icon: 'img/flags/no.svg',
+    mainStats: {
+      skillLevel: 'A1',
+      timeSpent: '16h 5m',
+      correctAnswers: 5,
+      incorrectAnswers: 15,
+      successRatio: 15,
+      totalCredits: 523,
+    },
+    otherStats: [
+      {
+        stat: 52,
+        description: 'CUSTOM STAT',
+      },
+      {
+        stat: 22,
+        description: 'CUsdfsdfSTOM STAT',
+      },
+      {
+        stat: 5082,
+        description: 'CUSTOM STAasvT',
+      },
+    ],
+  },
+
+  {
+    id: 6,
+    category: 'school-topic',
+    title: 'Biochemistry',
+    fullTitle: 'Biochemistry',
+    mainStats: {
+      skillLevel: 'EXPERT',
+      timeSpent: '15 min',
+      correctAnswers: 90,
+      incorrectAnswers: 245,
+      successRatio: 35,
+      totalCredits: 523,
+    },
+    otherStats: [
+      {
+        stat: 52,
+        description: 'CUSTOM STAT',
+      },
+      {
+        stat: 22,
+        description: 'CUsdfsdfSTOM STAT',
+      },
+      {
+        stat: 5082,
+        description: 'CUSTOM STAasvT',
+      },
+    ],
+  },
+  {
+    id: 7,
+    category: 'school-topic',
+    title: 'History',
+    fullTitle: 'History',
+    mainStats: {
+      skillLevel: 'BEGINNER',
+      timeSpent: '58 min',
+      correctAnswers: 114,
+      incorrectAnswers: 39,
+      successRatio: 25,
+      totalCredits: 523,
+    },
+    otherStats: [
+      {
+        stat: 52,
+        description: 'CUSTOM STAT',
+      },
+      {
+        stat: 22,
+        description: 'CUsdfsdfSTOM STAT',
+      },
+      {
+        stat: 5082,
+        description: 'CUSTOM STAasvT',
+      },
+    ],
+  },
+
+  {
+    id: 8,
+    category: 'quiz',
+    title: 'A good quiz',
+    fullTitle: 'A good quiz',
+    mainStats: {
+      skillLevel: '44%',
+      timeSpent: '1h',
+      correctAnswers: 2,
+      incorrectAnswers: 63,
+      successRatio: 25,
+      totalCredits: 523,
+    },
+    otherStats: [
+      {
+        stat: 52,
+        description: 'CUSTOM STAT',
+      },
+      {
+        stat: 22,
+        description: 'CUsdfsdfSTOM STAT',
+      },
+      {
+        stat: 5082,
+        description: 'CUSTOM STAasvT',
+      },
+    ],
+  },
+  {
+    id: 9,
+    category: 'quiz',
+    title: 'Spanish history quiz',
+    fullTitle: 'Spanish history quiz',
+    mainStats: {
+      skillLevel: '27%',
+      timeSpent: '32 min',
+      correctAnswers: 53,
+      incorrectAnswers: 15,
+      successRatio: 15,
+      totalCredits: 523,
+    },
+    otherStats: [
+      {
+        stat: 52,
+        description: 'CUSTOM STAT',
+      },
+      {
+        stat: 22,
+        description: 'CUsdfsdfSTOM STAT',
+      },
+      {
+        stat: 5082,
+        description: 'CUSTOM STAasvT',
+      },
+    ],
+  },
+  {
+    id: 10,
+    category: 'quiz',
+    title: 'TV shows',
+    fullTitle: 'TV shows',
+    mainStats: {
+      skillLevel: '90%',
+      timeSpent: '31 min',
+      correctAnswers: 75,
+      incorrectAnswers: 35,
+      successRatio: 15,
+      totalCredits: 523,
+    },
+    otherStats: [
+      {
+        stat: 52,
+        description: 'CUSTOM STAT',
+      },
+      {
+        stat: 22,
+        description: 'CUsdfsdfSTOM STAT',
+      },
+      {
+        stat: 5082,
+        description: 'CUSTOM STAasvT',
+      },
+    ],
+  },
+  {
+    id: 11,
+    category: 'quiz',
+    title: 'Czech presidents',
+    fullTitle: 'Czech presidents',
+    mainStats: {
+      skillLevel: '20%',
+      timeSpent: '22h 15m',
+      correctAnswers: 5,
+      incorrectAnswers: 15,
+      successRatio: 15,
+      totalCredits: 523,
+    },
+    otherStats: [
+      {
+        stat: 52,
+        description: 'CUSTOM STAT',
+      },
+      {
+        stat: 22,
+        description: 'CUsdfsdfSTOM STAT',
+      },
+      {
+        stat: 5082,
+        description: 'CUSTOM STAasvT',
+      },
+    ],
+  },
+  {
+    id: 12,
+    category: 'quiz',
+    title: 'Difficult quiz about animals',
+    fullTitle: 'Difficult quiz about animals',
+    mainStats: {
+      skillLevel: '54%',
+      timeSpent: '1 min',
+      correctAnswers: 14,
+      incorrectAnswers: 25,
       successRatio: 15,
       totalCredits: 523,
     },
