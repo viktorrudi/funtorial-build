1eab26555c7cf0eeb3f2f0f2dfbe0b2bb7a3e161
Viktor Rudi <viktor@Rudey.local>
2019-05-22 20:30:02 +0200

    html templified discounts

--- js/discounts_page.js
+++ js/discounts_page.js
@@ -1,87 +1,99 @@
-'use strict';
+'use strict'
 
 const discountFunctions = () => {
   const seeMoreInfo = () => {
-    const btnsSeeMore = document.querySelectorAll('.discount__btn--see-more');
+    const btnsSeeMore = document.querySelectorAll('.discount__btn--see-more')
     // const btnClaim = document.querySelector('.discount__btn--claim');
 
-    const btnArray = Array.from(btnsSeeMore);
+    const btnArray = Array.from(btnsSeeMore)
     btnArray.map(btnSeeMore => {
       btnSeeMore.addEventListener('click', e => {
         // Closest content box in relation to the button clicked
-        const discountContent = btnSeeMore.parentElement.parentElement.querySelector('.discount__header--content');
+        const discountContent = btnSeeMore.parentElement.parentElement.querySelector(
+          '.discount__header--content'
+        )
 
         // When closing discount list
         if (discountContent.classList.contains('extended')) {
-          discountContent.classList.remove('extended');
+          discountContent.classList.remove('extended')
           /* html */
-          btnSeeMore.innerHTML = `<i class="fas fa-angle-down"></i> See more details`;
+          btnSeeMore.innerHTML = `<i class="fas fa-angle-down"></i> See more details`
         }
         // When opening discount list
         else {
-          discountContent.classList.add('extended');
+          discountContent.classList.add('extended')
           /* html */
-          btnSeeMore.innerHTML = `<i class="fas fa-angle-up"></i> See less details`;
+          btnSeeMore.innerHTML = `<i class="fas fa-angle-up"></i> See less details`
         }
-      });
-    });
-  };
+      })
+    })
+  }
 
   const modalFunc = () => {
     const contentModal = document.querySelector('.modal__content'),
       behindModal = document.querySelector('.modal__background'),
       closeBtn = document.querySelector('.modal__close'),
-      claimBtns = document.querySelectorAll('.discount__btn--claim');
+      claimBtns = document.querySelectorAll('.discount__btn--claim')
 
     const closeModal = () => {
       const close = () => {
         //Adding remove-classes to modal and its background
-        contentModal.classList.add('modal__closed--content');
-        behindModal.classList.add('modal__closed--behind');
-      };
-      closeBtn.addEventListener('click', () => close());
-      behindModal.addEventListener('click', () => close());
-      return true;
-    };
+        contentModal.classList.add('modal__closed--content')
+        behindModal.classList.add('modal__closed--behind')
+      }
+      closeBtn.addEventListener('click', () => close())
+      behindModal.addEventListener('click', () => close())
+      return true
+    }
 
     const openModal = () => {
       const open = e => {
-        contentModal.classList.remove('modal__closed--content');
-        behindModal.classList.remove('modal__closed--behind');
-        const discountInfo = JSON.parse(e.srcElement.getAttribute('data-discount-info'));
+        contentModal.classList.remove('modal__closed--content')
+        behindModal.classList.remove('modal__closed--behind')
+        const discountInfo = JSON.parse(e.srcElement.getAttribute('data-discount-info'))
 
-        // Helper function for below actions
+        // Helper function for the preview inside the modal
         const putDiscount = (selector, value) => {
-          document.querySelector(selector).innerHTML = discountInfo[value];
-        };
+          document.querySelector(selector).innerHTML = discountInfo[value]
+        }
+
+        // Helper function to put data to hidden inputs
+        const putDiscountInput = (name, value) => {
+          document.querySelector(`input[name=${name}]`).setAttribute('value', discountInfo[value])
+        }
 
         // Info picked up from discounts-enter
-        putDiscount('.old-currency-view', 'currency');
-        putDiscount('.new-currency-view', 'currency');
-        putDiscount('.name-view', 'name');
-        putDiscount('.credit-cost', 'creditCost');
-        putDiscount('.price-now', 'priceNew');
-        putDiscount('.price-before', 'priceOld');
-        putDiscount('.percent-view', 'percent');
-        return true;
-      };
+        putDiscount('.old-currency-view', 'currency')
+        putDiscount('.new-currency-view', 'currency')
+        putDiscount('.name-view', 'name')
+        putDiscount('.credit-cost', 'creditCost')
+        putDiscount('.price-now', 'priceNew')
+        putDiscount('.price-before', 'priceOld')
+        putDiscount('.percent-view', 'percent')
+
+        // Adding information to hidden text fields
+        putDiscountInput('provider-email', 'providerEmail')
+        putDiscountInput('provider-message', 'providerMessage')
+
+        return true
+      }
 
       //Add click event listener to all claim buttons
       claimBtns.forEach(btn => {
         btn.addEventListener('click', e => {
-          open(e);
-        });
-      });
-    };
-    openModal();
-    closeModal();
-  };
+          open(e)
+        })
+      })
+    }
+    openModal()
+    closeModal()
+  }
 
   //Run functions on page load
   window.addEventListener('DOMContentLoaded', () => {
-    seeMoreInfo();
-    modalFunc();
-  });
-};
+    seeMoreInfo()
+    modalFunc()
+  })
+}
 
-discountFunctions();
+discountFunctions()
