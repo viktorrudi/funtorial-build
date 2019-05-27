fab3d2e3c2de35cdd0f554cd3b6be2708fe09578
Viktor Rudi <viktor@Rudey.local>
2019-05-27 08:12:42 +0200

    html templatifieed navbar and roopics

--- js/insert_data/language_selected.js
+++
@@ -1,9 +0,0 @@
-export default [
-  {
-    id: 1,
-    name: 'English',
-    icon: './img/flags/gb.svg',
-    countryCode: 'en',
-    link: '#',
-  },
-];
--- js/topics_import.js
+++ js/topics_import.js
@@ -1,46 +1,46 @@
-'use strict';
+'use strict'
 
-import data from './insert_data/available_exercises.js';
+import data from './insert_data/available_exercises.js'
 class Topic {
   constructor(obj) {
-    this.id = obj.id;
-    this.chosen = obj.chosen;
-    this.title = obj.title;
-    this.fullTitle = obj.fullTitle || '';
-    this.category = obj.category;
-    this.icon = obj.icon;
-    this.link = obj.link;
+    this.id = obj.id
+    this.chosen = obj.chosen
+    this.title = obj.title
+    this.fullTitle = obj.fullTitle || ''
+    this.category = obj.category
+    this.icon = obj.icon
+    this.link = obj.link
   }
   build() {
     // Sorts out to only display exercises which are set to chosen and on the right page
     if (!document.querySelector('.selection__new-topics') && this.chosen === false) {
-      return;
+      return
     }
 
     // Initialize variable for which block to put the exercise into. Logic in if-clause
-    let selectorToUse;
+    let selectorToUse
 
     if (this.category == 'language') {
-      selectorToUse = document.querySelector('#category__feed--lang');
+      selectorToUse = document.querySelector('#category__feed--lang')
     } else if (this.category == 'school-topic') {
-      selectorToUse = document.querySelector('#category__feed--school');
+      selectorToUse = document.querySelector('#category__feed--school')
     } else if (this.category == 'quiz') {
-      selectorToUse = document.querySelector('#category__feed--quiz');
+      selectorToUse = document.querySelector('#category__feed--quiz')
     } else {
-      console.error('Topic category not matching. Use language, school-topic or quiz.');
+      console.error('Topic category not matching. Use language, school-topic or quiz.')
     }
 
-    const topicWrapper = document.createElement('div');
-    topicWrapper.setAttribute('class', 'category__item');
+    const topicWrapper = document.createElement('div')
+    topicWrapper.setAttribute('class', 'category__item')
 
     // If no flag is in object, dont add it, which will use the language color for the icon
-    let icon = '';
+    let icon = ''
     if (this.icon) {
-      icon = `<object data="${this.icon}" type="image/svg+xml"></object>`;
+      icon = `<object data="${this.icon}" type="image/svg+xml"></object>`
     }
 
     // Output setup
-    let toFeed = ``;
+    let toFeed = ``
     /* html */
     toFeed += `
     <a data-topic-id="${this.id}" 
@@ -52,12 +52,12 @@ class Topic {
       </span>
       <p>${this.title}</p>
     </a>
-  `;
-    topicWrapper.innerHTML = toFeed;
-    selectorToUse.appendChild(topicWrapper);
+  `
+    topicWrapper.innerHTML = toFeed
+    selectorToUse.appendChild(topicWrapper)
   }
 }
 // Inserts each exercise into the page
 data.forEach(exercise => {
-  new Topic(exercise).build();
-});
+  new Topic(exercise).build()
+})
