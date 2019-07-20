42a88d86d8c9ae034c4b9a606125e9ff6e6849c2
Viktor Rudi <viktor@Rudey.local>
2019-07-20 17:29:03 +0200

    faq + discount providers HTML templates

--- elearning-template/src/js/app.js
+++ elearning-template/src/js/app.js
@@ -1,13 +1,22 @@
 // html
 import '../../index.html'
 import '../../contact.html'
-import '../../discounts.html'
+import '../../support-us.html'
+import '../../work-with-us.html'
+import '../../faq.html'
+import '../../text-pages.html'
+import '../../discount-providers.html'
+import '../../exercise-default.html'
+import '../../exercise-long-question.html'
+import '../../exercise-text-answer.html'
 import '../../exercise-end.html'
 import '../../register.html'
+import '../../login.html'
 import '../../statistics.html'
 import '../../text-pages.html'
 import '../../topic-selection-chosen.html'
 import '../../topic-selection.html'
+import '../../edit-profile.html'
 
 // scss
 import '../scss/main.scss'
@@ -45,3 +54,4 @@ $(document).ajaxComplete(function() {
 import './question.js'
 import './discounts.js'
 import './finishedResults'
+import './template/forms.js'
